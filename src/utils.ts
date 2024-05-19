d4e3b4961c00545af90658b0547860c0b0bbe391
Jacob Kim <jpoly1219@gmail.com>
2024-05-19 16:51:10 -0400

    conform to TS type system

--- src/utils.ts
+++ src/utils.ts
@@ -1,44 +1,44 @@
-const indexOfRegexGroup = (match, n) => {
+const indexOfRegexGroup = (match: RegExpMatchArray, n: number) => {
   return match.reduce((acc, curr, i) => {
     if (i < 1 || i >= n) return acc;
     return acc + curr.length;
-  }, match.index)
+  }, match.index as number)
 }
 
-const formatTypeSpan = (typeSpan) => {
+const formatTypeSpan = (typeSpan: string) => {
   return typeSpan.split("").reduce((acc, curr) => {
     if ((curr === "\n" || curr === " ") && acc.slice(-1) === " ") return acc;
     return acc + curr;
   }, "")
 }
 
-const isTuple = (typeSpan) => {
+const isTuple = (typeSpan: string) => {
   return typeSpan[0] === "[" && typeSpan[typeSpan.length - 1] === "]";
 }
 
-const isUnion = (typeSpan) => {
+const isUnion = (typeSpan: string) => {
   return typeSpan.includes(" | ");
 }
 
-const isArray = (typeSpan) => {
+const isArray = (typeSpan: string) => {
   return typeSpan.slice(-2) === "[]";
 }
 
-const isObject = (typeSpan) => {
+const isObject = (typeSpan: string) => {
   return typeSpan[0] === "{" && typeSpan[typeSpan.length - 1] === "}";
 }
 
 // this is a very rudimentary check, so it should be expanded upon
-const isFunction = (typeSpan) => {
+const isFunction = (typeSpan: string) => {
   return typeSpan.includes("=>");
 }
 
-const isPrimitive = (typeSpan) => {
+const isPrimitive = (typeSpan: string) => {
   const primitives = ["string", "number", "boolean"];
   return primitives.includes(typeSpan);
 }
 
-const isTypeAlias = (typeSpan) => {
+const isTypeAlias = (typeSpan: string) => {
   const caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return caps.includes(typeSpan[0]);
 }
