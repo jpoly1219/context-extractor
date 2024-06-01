61895eb5f3966b5329621dc6e2b1f2257eda73e9
Jacob Kim <jpoly1219@gmail.com>
2024-06-01 14:32:43 -0400

    extract vars

--- src/codeql.ts
+++ src/codeql.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
 import { execSync } from "child_process";
-import { parseCodeQLTable } from "./utils";
-import { relevantTypeObject } from "./types";
+import { parseCodeQLRelevantTypes, parseCodeQLVars } from "./utils";
+import { relevantTypeObject, varsObject } from "./types";
 
 const extractRelevantTypes = (pathToCodeQL: string, pathToQuery: string, pathToDatabase: string, outDir: string): Map<string, relevantTypeObject> => {
   const pathToBqrs = path.join(outDir, "relevant-types.bqrs");
@@ -22,13 +22,35 @@ const extractRelevantTypes = (pathToCodeQL: string, pathToQuery: string, pathToD
   }
 
   const relevantTypesContent = fs.readFileSync(pathToDecodedTxt);
-  const relevantTypes = parseCodeQLTable(relevantTypesContent.toString());
+  const relevantTypes = parseCodeQLRelevantTypes(relevantTypesContent.toString());
 
   return relevantTypes;
 }
 
-const extractRelevantContext = () => {
+const extractVars = (pathToCodeQL: string, pathToQuery: string, pathToDatabase: string, outDir: string): Map<string, varsObject> => {
+  const pathToBqrs = path.join(outDir, "vars.bqrs");
+  const pathToDecodedTxt = path.join(outDir, "vars.txt");
 
+  // run CodeQL query vars.ql
+  try {
+    execSync(`${pathToCodeQL} query run ${pathToQuery} --database=${pathToDatabase} --output=${pathToBqrs}`);
+  } catch (err) {
+    console.error(`error while running query ${pathToQuery}: ${err}`);
+  }
+
+  try {
+    execSync(`${pathToCodeQL} bqrs decode ${pathToBqrs} --format=text --output=${pathToDecodedTxt}`);
+  } catch (err) {
+    console.error(`error while trying to decode ${outDir}/vars.bqrs: ${err}`);
+  }
+
+  const varsContent = fs.readFileSync(pathToDecodedTxt);
+  const vars = parseCodeQLVars(varsContent.toString());
+
+  return vars;
+}
+
+const extractRelevantContext = (vars: Map<string, varsObject>) => {
 }
 
 const extractRelevantContextHelper = () => {
