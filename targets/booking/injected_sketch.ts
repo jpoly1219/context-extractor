ab8fa3a1eef4964c21fe30a458198905efb1245a
Jacob Kim <jpoly1219@gmail.com>
2024-06-20 22:47:54 -0400

    removing injected files

--- targets/booking/injected_sketch.ts
+++
@@ -1,8 +0,0 @@
-declare function _(): (model: Model, action: Action) => Model
-import { Model, Action } from "./prelude";
-
-// Update the Room Booking app model based on an action
-const update: (model: Model, action: Action) => Model =
-  _()
-
-export { update };
