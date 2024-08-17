832e1f5cbd1c2eadbf0dbfdd2e676ce5e42c7c06
Jacob Kim <jpoly1219@gmail.com>
2024-08-17 14:10:17 -0400

    style

--- targets/ocaml/todo/prelude.ml
+++ targets/ocaml/todo/prelude.ml
@@ -20,20 +20,19 @@ type action =
   | UpdateBuffer of string
 
 (* type Update = (Model, Action) -> Model in *)
-type update = (model * action) -> model
+type update = model * action -> model
 
 (* let Todo.eq: (Todo, Todo) -> Bool = *)
 (*   fun (d1, s1), (d2, s2) -> *)
 (*   d1 $== d2 && bool_eq(s1, s2) in *)
-let todo_eq (((d1, s1), (d2, s2)) : todo * todo) : bool =
-  d1 = d2 && s1 = s2
+let todo_eq (((d1, s1), (d2, s2)) : todo * todo) : bool = d1 = d2 && s1 = s2
 
 (* let Model.eq: (Model, Model) -> Bool = *)
 (*   fun (b1, ts1), (b2, ts2) -> *)
 (*     b1 $== b2 && List.equal(Todo.eq, ts1, ts2) in *)
 (* TODO: can't use todo_eq, as the comparator is a 'a -> 'a -> bool, not 'a * 'a -> bool *)
-let model_eq (((b1, ts1), (b2, ts2)): model * model) : bool =
-  b1 = b2 && (List.equal (fun (d1, s1) (d2, s2) -> d1 = d2 && s1 = s2) ts1 ts2)
+let model_eq (((b1, ts1), (b2, ts2)) : model * model) : bool =
+  b1 = b2 && List.equal (fun (d1, s1) (d2, s2) -> d1 = d2 && s1 = s2) ts1 ts2
 
 (* let Model.init: Model = ("", []) in *)
 let model_init : model = ("", [])
@@ -59,4 +58,7 @@ let remove ((index, todos) : int * todo list) : todo list =
 (*         (description, if i == index then !done else done), *)
 (*         todos) in *)
 let toggle ((index, todos) : int * todo list) : todo list =
-  List.mapi (fun i (description, finished) -> (description, if i = index then not finished else finished)) todos
+  List.mapi
+    (fun i (description, finished) ->
+      (description, if i = index then not finished else finished))
+    todos
