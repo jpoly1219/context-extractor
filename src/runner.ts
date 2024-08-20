7cb18fd7aace27cb104e69ad5d15f29ed44384bd
Jacob Kim <jpoly1219@gmail.com>
2024-08-20 17:50:56 -0400

    move the app running code here

---
+++ src/runner.ts
@@ -0,0 +1,8 @@
+import { extract, extractWithNew } from "./main";
+import { Language } from "./types";
+// extract("/home/jacob/projects/context-extractor/targets/todo/sketch.ts").then(r => console.log("todo\n", r));
+// extract("/home/jacob/projects/context-extractor/targets/playlist/sketch.ts").then(r => console.log("playlist\n", r));
+// extract("/home/jacob/projects/context-extractor/targets/passwords/sketch.ts").then(r => console.log("passwords\n", r));
+// extract("/home/jacob/projects/context-extractor/targets/booking/sketch.ts").then(r => console.log("booking\n", r));
+// extract("/home/jacob/projects/context-extractor/targets/emojipaint/sketch.ts").then(r => console.log("emojipaint\n", r));
+extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/todo/sketch.ts").then(r => console.log("todo\n", r));
