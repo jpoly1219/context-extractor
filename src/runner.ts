b197b39526f3836c36e392f47c629d3717c71742
jpoly1219 <jpoly1219@gmail.com>
2024-12-31 17:43:10 +0900

    add termination

--- src/runner.ts
+++ src/runner.ts
@@ -1,16 +1,47 @@
-import { extract, extractWithNew } from "./main";
-import { Language } from "./types";
+import { extract, extractContext } from "./main";
+import { Context, Language } from "./types";
+
 // extract("/home/jacob/projects/context-extractor/targets/todo/sketch.ts").then(r => console.log("todo\n", r));
 // extract("/home/jacob/projects/context-extractor/targets/playlist/sketch.ts").then(r => console.log("playlist\n", r));
 // extract("/home/jacob/projects/context-extractor/targets/passwords/sketch.ts").then(r => console.log("passwords\n", r));
 // extract("/home/jacob/projects/context-extractor/targets/booking/sketch.ts").then(r => console.log("booking\n", r));
 // extract("/home/jacob/projects/context-extractor/targets/emojipaint/sketch.ts").then(r => console.log("emojipaint\n", r));
-extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/todo/sketch.ts", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("todo\n", r));
+
+(async () => {
+  try {
+    const x = await extractContext(
+      Language.TypeScript,
+      "/home/jacob/projects/context-extractor/targets/todo/sketch.ts",
+      "/home/jacob/projects/context-extractor/targets/todo/",
+      "/home/jacob/projects/context-extractor/credentials.json",
+      false
+    )
+    console.dir(x, { depth: null })
+
+    const y = await extractContext(
+      Language.OCaml,
+      "/home/jacob/projects/context-extractor/targets/ocaml/todo/sketch.ml",
+      "/home/jacob/projects/context-extractor/targets/ocaml/todo/",
+      "/home/jacob/projects/context-extractor/credentials.json",
+      false
+    );
+    console.dir(y, { depth: null })
+  } finally {
+    // Debug active handles if the app doesn't terminate
+    // if ((process as any)._getActiveHandles().length > 0) {
+    //   console.log("Active handles detected:");
+    //   console.log((process as any)._getActiveHandles());
+    // }
+    process.exit(0);
+  }
+})();
+
+
 // extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/playlist/sketch.ts", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("playlist\n", r));
 // extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/passwords/sketch.ts", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("passwords\n", r));
 // extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/booking/sketch.ts", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("booking\n", r));
 // extractWithNew(Language.TypeScript, "/home/jacob/projects/context-extractor/targets/emojipaint/sketch.ts", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("emojipaint\n", r));
-extractWithNew(Language.OCaml, "/home/jacob/projects/context-extractor/targets/ocaml/todo/sketch.ml", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("todo\n", r));
+// extractWithNew(Language.OCaml, "/home/jacob/projects/context-extractor/targets/ocaml/todo/sketch.ml", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("todo\n", r));
 // extractWithNew(Language.OCaml, "/home/jacob/projects/context-extractor/targets/ocaml/playlist/sketch.ml", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("playlist\n", r));
 // extractWithNew(Language.OCaml, "/home/jacob/projects/context-extractor/targets/ocaml/passwords/sketch.ml", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("passwords\n", r));
 // extractWithNew(Language.OCaml, "/home/jacob/projects/context-extractor/targets/ocaml/booking/sketch.ml", "/home/jacob/projects/context-extractor/credentials.json").then(r => console.log("booking\n", r));
